16513bc480786cb0a8a821a4182675794ca542ed
James Winston <kenjameswinston@gmail.com>
2022-05-20 10:48:32 -0400

    Add capabilities to save progress to file in real time

--- card.py
+++ card.py
@@ -9,54 +9,111 @@ BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("ariel", 40, "italic")
 FLASH_FONT = ("ariel", 60, "bold")
 
+# So that we can kill our after() loops in any method
+global after_id
+
 
 class Cards(PhotoImage, Canvas):
     def __init__(self):
         super().__init__()
-        # Read in CSV and create dataframe
-        self.cards_data = pd.read_csv("./data/french_words.csv")
-        self.cards_df = pd.DataFrame(self.cards_data)
-
-        # Convert df to list
-        self.cards = self.cards_df.values.tolist()
-
-        # Shuffle deck
-        random.shuffle(self.cards)
+        self.cards = self.load_cards()
 
         # Choose last card from deck
         self.card_number = len(self.cards) - 1
         self.current_card = self.cards[self.card_number]
 
-        # Load background images
+        # Assigned words that user already knows
+        self.removed_list = []
+
+        # Load background for front and back of card
         self.question_image = PhotoImage(file="./images/card_front.png")
         self.answer_image = PhotoImage(file="./images/card_back.png")
 
-        # Draw and position card image canvas
+        # Draw and position main card canvas
         self.canvas = Canvas(height=CARD_HEIGHT, width=CARD_WIDTH, bg=BACKGROUND_COLOR)
         self.canvas.grid(row=0, column=0, columnspan=2)
         self.image_item = self.canvas.create_image(0, 0, image=self.question_image, anchor=tkinter.NW)
 
-        # Initialize flash card text
+        # Draw the first words and the langauge title to screen
         self.title_item = self.canvas.create_text(400, 150, text="French", font=TITLE_FONT)
         self.flash_item = self.canvas.create_text(400, 263, text=self.cards[0][0], font=FLASH_FONT)
 
-    def flip_card(self, direction):
-        if direction == "back":
-            self.canvas.itemconfig(self.image_item, image=self.answer_image)
+    def show_questions(self):
 
-    def show_cards(self, end_loop=False):
-        # Pick the next card from deck
-        if self.card_number > 0:
-            self.card_number -= 1
-            self.current_card = self.cards[self.card_number]
+        global after_id
+        print("before update", self.current_card)
+        self.update_deck()
+        print("after update", self.current_card)
+        # Show the front of the card
+        self.canvas.itemconfig(self.flash_item, text=self.current_card[0], fill="black")
+        self.canvas.itemconfig(self.image_item, image=self.question_image)
+        self.canvas.itemconfig(self.title_item, text="French")
 
-            # Update flash text
-            self.canvas.itemconfig(self.flash_item, text=self.current_card[0], fill="black")
-            after_id = self.after(3000, self.show_cards, False)
-            # Execute this code if user clicks the red 'x' button to kill the loop
-            if end_loop:
-                self.after_cancel(after_id)
+        # Show the answer after 3 seconds
+        after_id = self.canvas.after(3000, self.show_answer)
+
+    def show_answer(self):
+
+        global after_id
+        # Show the back of the card
+        self.canvas.itemconfig(self.image_item, image=self.answer_image)
+        self.canvas.itemconfig(self.flash_item, text=self.current_card[1])
+        self.canvas.itemconfig(self.title_item, text="English")
+
+        # Show the next question
+        after_id = self.canvas.after(3000, self.show_questions)
 
     def next_card(self):
-        self.show_cards(end_loop=True)
+        # kill the current loop show the next question
+        global after_id
+        self.canvas.after_cancel(after_id)
+        self.show_questions()
 
+    def remove_word(self):
+        # Get current card and remove it from the list
+        current_card_index = self.cards.index(self.current_card)
+        # Add removed word to list
+        self.removed_list.append(self.cards.pop(current_card_index))
+        # Write removed words to csv file
+        self.write_to_file()
+        # Ensure we don't get any index errors
+        self.update_deck()
+        # Call this to avoid index errors if user presses button multiple times
+        self.next_card()
+
+    def update_deck(self):
+        self.card_number -= 1
+
+        # Update the current card
+        if self.card_number >= 0:
+            self.current_card = self.cards[self.card_number]
+        # Once the last card is indexed reset the deck and show them all again
+        else:
+            self.card_number = len(self.cards) - 1
+            self.current_card = self.cards[self.card_number]
+
+    def write_to_file(self):
+        words_to_learn = pd.DataFrame(self.cards, columns=['French', 'English'])
+        words_to_learn.to_csv("./data/words_to_learn.csv", index=False)
+
+    def load_cards(self):
+        try:
+            with open("./data/words_to_learn.csv", "r") as f:
+                pass
+
+        except FileNotFoundError:
+            cards_data = pd.read_csv("./data/french_words.csv")
+            cards_df = pd.DataFrame(cards_data)
+            # Convert df to list
+            cards = cards_df.values.tolist()
+        else:
+            cards_data = pd.read_csv("./data/words_to_learn.csv")
+            cards_df = pd.DataFrame(cards_data)
+            # Convert df to list
+            cards = cards_df.values.tolist()
+
+        finally:
+            random.shuffle(cards)
+
+        # Return a list of lists ie; [['oui', 'yes']]
+        return cards
--- main.py
+++ main.py
@@ -3,68 +3,10 @@ from card import Cards
 import random
 import pandas as pd
 
-
-
-
 BACKGROUND_COLOR = "#B1DDC6"
-# TITLE_FONT = ("ariel", 40, "italic")
-# FLASH_FONT = ("ariel", 60, "bold")
-# WINDOW_HEIGHT = 526
-# WINDOW_WIDTH = 800
-#
 PAD_Y = 50
 PAD_X = 50
-#
-# LANGUAGE = "French"
-#
-# cards_data = pd.read_csv("./data/french_words.csv")
-# cards = pd.DataFrame(cards_data)
-#
-#
-# def choose_card():
-#     # choose a random row from dataframe
-#     card = cards.sample()
-#     card = card.values.tolist()
-#
-#     return card
-#
-#
-# def display_question():
-#     global after_id
-#
-#     card = choose_card()
-#     question = card[0][0]
-#
-#     canvas.itemconfig(flash_item, text=question, fill="black")
-#     canvas.itemconfig(title_item, text="French", fill="black")
-#
-#     after_id = root.after(3000, display_answer, card)
-#
-#
-# def display_answer(current_card):
-#     global after_id
-#     # display the answer side of the card
-#     answer = current_card[0][1]
-#
-#     canvas.itemconfig(image_item, image=card_back_img)
-#     canvas.itemconfig(title_item, text="English", fill="white")
-#     canvas.itemconfig(flash_item, text=answer, fill="white")
-#
-#     after_id = root.after(3000, display_question)
-#
-#
-# def wrong():
-#     # go to the next word if they click the red x
-#     root.after_cancel(after_id)
-#     display_question()
-#
-#
-#
-# def right():
-#     # TODO if user clicks here, remove the word from current word list and save to `words_to_learn.csv`
-#
-#     pass
-#
+
 
 # --------UI Setup-----------------#
 
@@ -83,27 +25,13 @@ cards = Cards()
 wrong_img = PhotoImage(height=100, width=99, file="./images/wrong.png")
 wrong_button = Button(root, image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=cards.next_card)
 wrong_button.grid(row=1, column=0)
-#
-# right_img = PhotoImage(height=100, width=100, file="./images/right.png")
-# right_button = Button(root, image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=)
-# right_button.grid(row=1, column=1)
-# #
-# # SETUP CARD FACE
-# canvas = Canvas(root, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bg=BACKGROUND_COLOR)
-# canvas.grid(row=0, column=0, columnspan=2)
-#
-# card_face_img = PhotoImage(file="./images/card_front.png")
-# card_back_img = PhotoImage(file="./images/card_back.png")
-#
-# image_item = canvas.create_image(0, 0, image=card_face_img, anchor=NW)
-# title_item = canvas.create_text(400, 150, text=LANGUAGE, font=TITLE_FONT)
-# flash_item = canvas.create_text(400, 263, text="", font=FLASH_FONT)
-# # SETUP CARD
-#
+
+right_img = PhotoImage(height=100, width=100, file="./images/right.png")
+right_button = Button(root, image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=cards.remove_word)
+right_button.grid(row=1, column=1)
 
 # MAIN LOOP
 
 
-cards.show_cards()
-
+cards.show_questions()
 root.mainloop()
